4f94a1f7fd197e9ab64ea4979a734d93307c2e2c
Humberto Zapata <humberto.zaleon@gmail.com>
2020-06-25 13:08:31 -0500

    Configuración de CORS

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.set('port', process.env.PORT || 3100);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 
 // ==================== RUTAS ====================
